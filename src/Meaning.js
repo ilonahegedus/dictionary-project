d454070208154fb1c96859e5dffe021e01c7029b
ilonahegedus <88673740+ilonahegedus@users.noreply.github.com>
2022-05-09 22:08:16 +0200

    Correction - spelling
    based on error message

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,7 @@ export default function Meaning(props) {
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
 
-      {props.meaning.definitions.map(function (definiton, index) {
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
